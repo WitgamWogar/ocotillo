356926f7b7d4a71a268be729dcb7acf1514f3789
Tyler Otto <tyler.otto@vuria.com>
2020-11-06 14:41:33 -0700

    Plant "source" should not be an array.

--- src/plant/interfaces/plant.interface.ts
+++ src/plant/interfaces/plant.interface.ts
@@ -5,7 +5,7 @@ export interface Plant extends Document {
     readonly common_name: string;
     readonly nickname: string;
     readonly aquired_at: string;
-    readonly source: string[];
+    readonly source: string;
     readonly location: number;
     readonly acquired_at: Date;
 }

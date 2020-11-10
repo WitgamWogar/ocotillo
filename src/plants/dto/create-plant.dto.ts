d96eefd3ea1a13b63bee23ddb8bca4c49aee7ef0
Tyler Otto <tyler.otto@vuria.com>
2020-11-10 15:51:18 -0700

    Fix issue preventing common_name from being saved on plants.

--- src/plants/dto/create-plant.dto.ts
+++ src/plants/dto/create-plant.dto.ts
@@ -1,11 +1,10 @@
-import { Exclude } from 'class-transformer';
+import { Exclude, Expose } from 'class-transformer';
 import { IsEmail, IsNotEmpty, IsIn, IsDate, IsDateString } from 'class-validator';
 
 export class CreatePlantDTO {
   @IsNotEmpty()
   scientific_name: string;
-
-  @Exclude()
+  
   common_name: string;
 
   nickname: string;
